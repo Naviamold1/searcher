b97ba7f980593736f4c2a5a750da05fd0e57c450
Ilia Arkania <iliaarkania13@gmail.com>
2022-10-24 20:52:06 +0400

    updates!

--- main.py
+++ main.py
@@ -173,7 +173,7 @@ def sort():
 
 @app.post("/search-item/{item}")
 def search(item):
-    Search(item).ada()
+    Search(item).ee()
     sort()
     df = pd.read_csv('output.csv')
     df.to_json('output.json', orient='records')
